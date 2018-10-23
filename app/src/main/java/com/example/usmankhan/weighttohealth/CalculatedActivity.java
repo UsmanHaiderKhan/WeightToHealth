657f7635520ac469c6d38545e23549004dc98320
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-23 18:02:09 +0500

    Added New Activity in the Project

---
+++ app/src/main/java/com/example/usmankhan/weighttohealth/CalculatedActivity.java
@@ -0,0 +1,13 @@
+package com.example.usmankhan.weighttohealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_calculated );
+    }
+}
